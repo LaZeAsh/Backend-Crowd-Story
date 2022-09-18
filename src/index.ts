b3c74676cc64644c01b1c6a364394a652a5a19db
Ammar Ahmed <ammar.ahmed2203@gmail.com>
2022-09-17 21:57:49 -0400

    cors stuff

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,13 @@ app.use(express.json());
 //app.use(cors());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested, Content-Type, Accept Authorization");
+
+  if (req.method === "OPTIONS"){
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+    return res.status(200).json({})
+  }
   next();
 })
 
