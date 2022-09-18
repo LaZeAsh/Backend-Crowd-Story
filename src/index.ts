0f00c0d705b94b86c044c1e86a05a5e6b7808f4c
Ammar Ahmed <ammar.ahmed2203@gmail.com>
2022-09-17 21:46:07 -0400

    cors stuff

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,13 @@ import cors from "cors";
 mongo();
 
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 
 app.get('/stories', async (req: Request, res: Response) : Promise<void> => {  
   const { id } = req.query;
