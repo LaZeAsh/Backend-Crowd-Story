8813241734983e52b1236708eadbb1f865bc080e
Ammar Ahmed <ammar.ahmed2203@gmail.com>
2022-09-17 22:10:06 -0400

    removed packagelock + cors

--- src/index.ts
+++ src/index.ts
@@ -12,18 +12,18 @@ import cors from "cors";
 mongo();
 
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested, Content-Type, Accept Authorization");
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested, Content-Type, Accept Authorization");
 
-  if (req.method === "OPTIONS"){
-    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
-    return res.status(200).json({})
-  }
-  next();
-})
+//   if (req.method === "OPTIONS"){
+//     res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+//     return res.status(200).json({})
+//   }
+//   next();
+// })
 
 app.get('/stories', async (req: Request, res: Response) : Promise<void> => {  
   const { id } = req.query;
